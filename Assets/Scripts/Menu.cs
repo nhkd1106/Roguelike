51857e9803ea543a6a32801016c1d8eb12849d0e
Đào Tiến Dũng <2051063775@e.tlu.edu.vn>
2024-01-07 12:41:01 +0700

    button exit

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -14,10 +14,17 @@ public class c : MonoBehaviour
     }
 
     // Update is called once per frame
-   public void Exit()
-    {    
-         Application.Quit(0);
-    }
+   public void QuitGame()
+{
+    // save any game data here
+    #if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit();
+    #endif
+}
         
     
 }
